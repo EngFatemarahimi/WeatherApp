9410ffe56973c661d23212a67d53f4c28bf87e34
Fatema Rahimi <fatemarahimi996@gmail.com>
2024-01-12 10:35:03 +0430

    change image

--- src/Components/WeatherApp/WeatherApp.jsx
+++ src/Components/WeatherApp/WeatherApp.jsx
@@ -15,7 +15,7 @@ const WeatherApp = () => {
          
    let api_key ="34b7e49444e896488456cd92c8b71c6f";
 
-   const [wicon, setWicon] = useState(cloud_icon);
+   const [wicon,setWicon] = useState(cloud_icon);
    
   
     const search = async () => {
@@ -24,26 +24,54 @@ const WeatherApp = () => {
           {
             return 0;
           }
-         let url = `https://api.openweathermap.org/data/2.5/weather?q=${element[0].value}&units=Metric&appid=${api_key}`;
+         let url = `https://api.openweathermap.org/data/2.5/weather?q=${element[0].value}&London&units=Metric&appid=${api_key}`;
          
          let response = await fetch(url);
          let data = await response.json();
 
-         const humidity = document.getElementById("humidity-percent");
+         const humidity = document.getElementsByClassName("humidity-percent")
          const wind = document.getElementsByClassName("wind-rate");
          const temprature = document.getElementsByClassName("weather-temp");
          const location = document.getElementsByClassName("weather-location");
    
    
-        humidity[0].innerHTML = data.main.humidity+ " % ";
-        wind[0].innerHTML = data.wind.speed+ " km/h ";
-        temprature[0].innerHTML = data.main.temprature+ " c";
-        location[0].innerHTML = data.name.location;
+         humidity[0].innerHTML = data.main.humidity+ " % ";
+         wind[0].innerHTML = Math.floor (data.wind.speed)+ " km/h ";
+         temprature[0].innerHTML = Math.floor (data.main.temp)+ "°c";
+        location[0].innerHTML = data.name;
 
-        if(data.weather[0].icon==="04d" || data.weather[0].icon==="01n" ){
+        if(data.weather[0].icon==="01d" || data.weather[0].icon==="01n" )
+        {
              setWicon(clear_icon )
         }
+         else if(data.weather[0].icon==="02d" || data.weather[0].icon==="02n" )
+        {
+              setWicon(cloud_icon)
         }
+        else if(data.weather[0].icon==="03d" || data.weather[0].icon==="03n" )
+        {
+          setWicon(drizzle_icon)
+        }
+        else if(data.weather[0].icon==="04d" || data.weather[0].icon==="04n" )
+        {
+              setWicon(drizzle_icon)
+        }
+        else if(data.weather[0].icon==="09d" || data.weather[0].icon==="09n" )
+        {
+              setWicon(rain_icon)
+        }
+        else if(data.weather[0].icon==="10d" || data.weather[0].icon==="10n" )
+        {
+          setWicon(rain_icon)
+        }   
+        else if(data.weather[0].icon==="13d" || data.weather[0].icon==="13n" )
+            {
+              setWicon(snow_icon);
+            }  
+        else{
+          setWicon(clear_icon);
+        }    
+        }  
 
   return (
     <div className='container'>
@@ -56,7 +84,7 @@ const WeatherApp = () => {
      <div className='weather-image'>
      <img src={wicon} alt=""  />
      </div>
-     <div className='weather-temp'> 24 c </div>
+     <div className='weather-temp'> 24 °c </div>
      <div className='weather-location'> London </div>
      
      <div className=' date-container'>
@@ -68,7 +96,7 @@ const WeatherApp = () => {
          </div>
      </div>  
 
-     
+
      <div className='element'>
      <img src={ wind_icon } alt=" " className='icon' />
      <div className='data'>
